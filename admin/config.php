ee5a2875ef84c02e95e5deea7d698ae1a6934b35
Pardeep Verma <pardeep@fathershops.com>
2024-10-03 18:49:54 +0530

    added journal 3

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://site.pardeep.app/admin/');
+define('HTTP_CATALOG', 'https://site.pardeep.app/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://site.pardeep.app/admin/');
+define('HTTPS_CATALOG', 'https://site.pardeep.app/');
+
+// DIR
+define('DIR_APPLICATION', '/home4/anmolf8p/public_html/Websites/demo/pardeep.app/site.pardeep.app/admin/');
+define('DIR_SYSTEM', '/home4/anmolf8p/public_html/Websites/demo/pardeep.app/site.pardeep.app/system/');
+define('DIR_IMAGE', '/home4/anmolf8p/public_html/Websites/demo/pardeep.app/site.pardeep.app/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', '/home4/anmolf8p/public_html/Websites/demo/pardeep.app/site.pardeep.app/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'anmolf8p_sitepardeep');
+define('DB_PASSWORD', 'opf&U*wIDJF(');
+define('DB_DATABASE', 'anmolf8p_sitepardeep');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
